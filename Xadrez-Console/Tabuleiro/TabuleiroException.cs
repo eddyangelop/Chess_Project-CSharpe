6aac4584812a6d83c10fe0d7d3302e3e3c447b0e
eddyangelop <eddysoullive@gmail.com>
2022-11-16 20:29:17 -0300

    implementado excessao personalizada

---
+++ Xadrez-Console/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tabuleiro
+{
+    internal class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
